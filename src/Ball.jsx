41c44ce21c57039ee0df3f6309722014d9100ec2
ydk1204 <ejrbdi@naver.com>
2022-06-22 18:06:18 +0900

    ball 이동 방식 변경

--- src/Ball.jsx
+++ src/Ball.jsx
@@ -2,26 +2,40 @@ import React, { useEffect, useRef, useState } from "react";
 import styles from "./Ball.module.css";
 
 const Ball = ({ pos }) => {
-  let { x, y } = pos;
   const Ball = useRef();
-  const [ballLocationX, setBallLocationX] = useState();
+  const [ballLocation, setBallLocation] = useState(pos);
+
+  const changeBallPos = () => {
+    const ball = Ball && Ball.current;
+    moveBall();
+    ball.style.top = ballLocation.x + "px";
+    ball.style.left = ballLocation.y + "px";
+  };
+
+  const moveBall = () => {
+    let vx = ballLocation.x - 10;
+    let newBall = ballLocation;
+    newBall.x = vx;
+    setBallLocation(newBall);
+  };
 
   useEffect(() => {
     const ball = Ball.current;
+    const interval = setInterval(changeBallPos, 10);
+    setBallLocation(pos);
+
+    ball.style.top = pos.x + "px";
+    ball.style.left = pos.y + "px";
+
     setTimeout(() => {
       ball.style.display = "none";
+      clearInterval(interval);
     }, 1000);
 
     return;
   }, []);
 
-  return (
-    <div
-      ref={Ball}
-      className={styles.ball}
-      style={{ top: x + "px", left: y + "px" }}
-    />
-  );
+  return <div ref={Ball} className={styles.ball} />;
 };
 
 export default Ball;
